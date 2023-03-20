cad42f1446a11abbdee37f87a3ba45429b78665f
samaksh mittal <sam.mittal44@gmail.com>
2023-03-20 14:20:29 +0530

    feat: add schema for post,author and comment

---
+++ sanitybackend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'comment',
+  title: 'Comment',
+  type: 'documnet',
+  fields: [
+    {
+      name: 'postedBy',
+      title: 'Posted By',
+      type: 'postedBy',
+    },
+    {
+      name: 'comment',
+      title: 'Comment',
+      type: 'string',
+    },
+  ],
+}
