f2fc2ac2c1e781d2e338726cba73bb0514e22adf
samaksh mittal <sam.mittal44@gmail.com>
2023-03-20 14:10:25 +0530

    chore: add user schema in sanity BE

---
+++ sanitybackend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'user',
+  title: 'User',
+  type: 'documnet',
+  fields: [
+    {
+      name: 'userName',
+      title: 'User Name',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+    },
+  ],
+}
