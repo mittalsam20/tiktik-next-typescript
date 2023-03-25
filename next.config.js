a5050a8f96c5b924437198a9a25581a57961df9d
samaksh mittal <sam.mittal44@gmail.com>
2023-03-26 00:14:52 +0530

    feat: save user details in cookies and add google images domains in next config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { domains: [`media.licdn.com`] },
+  images: { domains: [`media.licdn.com`, `lh3.googleusercontent.com`] },
 };
 
 module.exports = nextConfig;
