94317c8b5dcb83cde8749d8f5a828236e4f92fd7
samaksh mittal <sam.mittal44@gmail.com>
2023-04-06 03:37:08 +0530

    chore: install spell check for english and correct mispelled tailwind css classes

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -52,7 +52,7 @@ const VideoCard: NextPage<Iprops> = ({ post }) => {
   return (
     <div className={"flex flex-col border-b-2 border-gray-200 pb-6"}>
       <div>
-        <div className={"flex gap-3 p-2 curosr-pointer font-semibold rounded"}>
+        <div className={"flex gap-3 p-2 cursor-pointer font-semibold rounded"}>
           <div className={"md:w-16 md:h-16 w-10 h-10"}>
             <Link href={`/profile/${post.postedBy._id}`}>
               <>
@@ -67,9 +67,9 @@ const VideoCard: NextPage<Iprops> = ({ post }) => {
               </>
             </Link>
           </div>
-          <div className={""}>
-            <Link href={"/"}>
-              <div className={"flex items-cetner gap-2"}>
+          <div>
+            <Link href={`/profile/${post.postedBy._id}`}>
+              <div className={"flex items-center gap-2"}>
                 <p
                   className={
                     "flex gap-2 items-center md:text-md font-bold text-primary"
@@ -90,7 +90,7 @@ const VideoCard: NextPage<Iprops> = ({ post }) => {
           </div>
         </div>
       </div>
-      <div className={"lg:ml-20 felx gap-4 relative"}>
+      <div className={"lg:ml-20 flex gap-4 relative"}>
         <div
           className={"rounded-3xl"}
           onMouseEnter={() => setIsHover(true)}
@@ -110,7 +110,7 @@ const VideoCard: NextPage<Iprops> = ({ post }) => {
           {isHover && (
             <div
               className={
-                "absolute bottom-6 curosr-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] p-3"
+                "absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] p-3"
               }
             >
               {playing ? (
