09d1d5e9cfd0f1ba51a772d10d40b4b8a2698feb
samaksh mittal <sam.mittal44@gmail.com>
2023-04-06 16:49:52 +0530

    chore: major css fix in comments section

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -6,9 +6,9 @@ import Image from "next/legacy/image";
 import { GoVerified } from "react-icons/go";
 import { BiCommentX } from "react-icons/bi";
 
+import { IUser } from "@/types";
 import NoResults from "./NoResults";
 import useAuthStore from "@/store/authStore";
-import { IUser } from "@/types";
 
 interface IProps {
   comment: string;
@@ -22,37 +22,38 @@ interface IComment {
   comment: string;
   length?: number;
   _key: string;
-  postedBy: { _ref: string; _type?: string };
+  postedBy: { _ref?: string; _type?: string };
 }
 
 const Comments = ({
   comment,
   comments,
-  isPostingComment,
-  addComment,
   setComment,
+  addComment,
+  isPostingComment,
 }: IProps) => {
   const { userProfile, allUsers } = useAuthStore();
+  console.log(comments);
 
   return (
-    <div className="border-t-2 border-gray-200 pt-4 px-10 bg--[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
-      <div className="overflow-scroll lg:h-[475px]">
+    <div className="border-t-2 border-gray-200 pt-4 px-10 mt-4 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
+      <div className="overflow-scroll lg:h-[457px]">
         {comments?.length ? (
-          comments?.map((item: IComment, idx: number) =>
+          comments?.map((item: IComment, index: number) =>
             allUsers?.map(
               (user: IUser) =>
                 user._id === item.postedBy._ref && (
-                  <div className=" p-2 items-center" key={idx}>
+                  <div className=" p-2 items-center" key={index}>
                     <Link href={`/profile/${user._id}`}>
                       <div className="flex items-start gap-3">
                         <div className="w-12 h-12">
                           <Image
                             width={48}
                             height={48}
-                            className="rounded-full cursor-pointer"
                             src={user.image}
                             alt="user-profile"
                             layout="responsive"
+                            className="rounded-full cursor-pointer"
                           />
                         </div>
 
@@ -79,15 +80,15 @@ const Comments = ({
         )}
       </div>
       {userProfile && (
-        <div className="absolute bottom-0 left-0 pb-6 px-2 md:px-10">
-          <form onSubmit={addComment} className="flex gap-4 ">
+        <div className="absolute bottom-0 left-0  pb-6 px-2 md:px-10 ">
+          <form onSubmit={addComment} className="flex gap-4">
             <input
               value={comment}
+              placeholder={"Add comment.."}
               onChange={(e) => setComment(e.target.value.trim())}
-              placeholder={"Add Comment..."}
               className="bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[700px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg"
             />
-            <button className="text-md text-gray-400" onClick={addComment}>
+            <button className="text-md text-gray-400 " onClick={addComment}>
               {isPostingComment ? "Commenting..." : "Comment"}
             </button>
           </form>
